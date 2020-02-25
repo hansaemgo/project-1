f73f8e740145c724e6d61cbd55fcb1eccdbc1ede
hansaemgo <rhgkstoa7@gmail.com>
2020-02-25 11:53:28 +0900

    JavaScript-Practice
    0225-variation

---
+++ README.md
@@ -0,0 +1,10 @@
+# JavaScript 입문
+
+## 01. 변수
+
+- 특정 이름에 특정 값을 담을 때 사용
+
+  ```javascript
+  let value = 1;
+  console.log(value);
+  ```
--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import "./styles.css";
+console.log("hello");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let value = "1";
+console.log(value);
